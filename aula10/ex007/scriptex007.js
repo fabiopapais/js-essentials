ca27348ea2413fe42a099d712726a3674b0355b7
Fábio Papais <56613389+fabiopapaiss@users.noreply.github.com>
2019-12-28 16:33:28 -0300

    Delete scriptex007.js

--- aula10/ex007/scriptex007.js
+++
@@ -1,9 +0,0 @@
-function somar(){
-    var n1 = window.document.getElementById("input1")
-    var n2 = window.document.getElementById("input2")
-    var res = window.document.getElementById("res")
-    var nu1 = Number(n1.value)
-    var nu2 = Number(n2.value) 
-    soma = nu1 + nu2
-    res.innerHTML = `Resultado: <b>${soma}</b>`
-}    
