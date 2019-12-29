9fa5a5b2aa105b22f996e6f3907334666e7e0442
Fábio Papais <56613389+fabiopapaiss@users.noreply.github.com>
2019-12-29 20:04:01 -0200

    Add files via upload

--- aula10/ex006/scriptex006.js
+++ aula10/ex006/scriptex006.js
@@ -1,19 +1,25 @@
 var area = window.document.getElementById("area") // "Transformamos" nossa tag em uma variável para que possamos usa-lá em nosso código
 //Adicionando eventListeners para realizarem uma ação quando algo acontecer
-area.addEventListener("click", clicar)
-area.addEventListener("mouseenter", entrar)
-area.addEventListener("mouseout", sair)
+area.addEventListener("click", click)
+area.addEventListener("mouseenter", enter)
+area.addEventListener("mouseout", out)
 
-function entrar() {
+function enter() {
     area.innerText = 'Interagiu!'
-    }
+}
 
-function sair() {
+function out() {
     area.innerText = "Interaja..."
-    area.style.background = "rgb(255, 0, 0)"
+    /*area.style.background = "rgb(255, 0, 0)"*/
+}
+
+function click() { // Function for future improvement, try to make the colors (red and green) alternate everytime i click the mouse
+    area.innerText = "clicou!"
+    if (area.style.background == 'green') {
+        area.style.background == 'red'
+    }
+    if (area.style.background == 'red') {
+        area.style.background == 'green'
     }
 
-function clicar() {
-    area.innerText = "clicou!"
-    area.style.background = "green"
-        }
+}
