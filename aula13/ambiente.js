e6e5a3b061ba381fbc80f0e6e79e2ec5ca18aba7
Fábio Papais <56613389+fabiopapaiss@users.noreply.github.com>
2020-01-03 22:43:25 -0300

    Repetições

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+
+function testeNoInicio {
+    var i = 0
+
+    while (c < 6) {
+        console.log('olá')
+        i++
+    }
+}
+
+function testeNoFinal{
+    var i = 0
+
+    do {
+        console.log('olá')
+        i++
+    } while (c < 6)
+}
